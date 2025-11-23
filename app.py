6cc23df748cc7dc21f2a132481dc0eb29b7901e0
“Sergei007” <oni007@mail.ru>
2025-11-23 19:32:41 +0800

    Четвертый коммит

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ def index():
     return render_template('index.html',
                          in_stock_cars=cars_in_stock,
                          on_order_cars=cars_on_order,
-                         cars=cars_data)
+                         cars=cars_data,
+                         cities=cities_data)
 
 @app.route('/car/<car_id>')
 def car_detail(car_id):
     car = cars_data.get(car_id)
     if car:
         template_name = f'cars/{car_id.replace("-", "_")}.html'
-        return render_template(template_name, car=car, car_id=car_id)
+        return render_template(template_name, car=car, car_id=car_id, cities=cities_data)  # Добавляем cities
     else:
         return "Автомобиль не найден", 404
 
